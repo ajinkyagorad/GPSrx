807c1d63eb9331eee3498ab8e3b977125794eb2e
ajinkyagorad <ajinkyagorad@gmail.com>
2016-06-08 02:06:29 +0530

    wrong update of 1023, should be 20 :o

--- DataCorrCA.m
+++ DataCorrCA.m
@@ -1,8 +1,10 @@
 %Correlate GPS data with the C/A codes
 % read data from file
+clear
+
 fID=fopen('data');
 %1.79 GB ~ 20 sec
-time = 10E-3;
+time = 30E-3;
 bytesPerSec= (1.79*1024*1024*1024)/20;
 bytes = time*bytesPerSec;
 readSize = [2 floor(bytes/2)];
@@ -13,30 +15,30 @@ data=fread(fID,readSize);
 A = data';
 % B has the complex actual sampled data by SDR/USRP
 B = A(:,1)+i*A(:,2);
-figure(1)
-title('Correlation with code')
-xlabel('index')
-ylabel('correlated value')
-figure(2)
-%title('Filtered Response')
-%xlabel('index')
-%ylabel('avg Value')
+ figure(1)
+ title('Correlation with code')
+ xlabel('index')
+ ylabel('correlated value')
+% figure(2)
+% title('Filtered Response')
+% xlabel('index')
+% ylabel('avg Value')
 %Add a filter parameters
-N = 300;
+N = 3000;
 %figure(3)
 %stitle('avg input signal')
 %plot(smooth(abs(B),2000));
-for k=1:28
+for k=1:2
     g = cacode([k],10/1.023);   % doing at 10Mhz, c/a code sampling
-    g1023 = repmat(g,1,1023);
-    r = xcorr(B,g1023);
+    g20 = repmat(g,1,20);
+    r = xcorr(B,g20);
     
     %r_f = smooth(r,N);
-    figure(1);
-    plot(abs(r));
-    hold on
+     figure(1);
+     plot(abs(r));
+     hold on
     
-    %figure(2)
-    %plot(abs(r_f));
-    %hold on
+%      figure(2)
+%      plot(abs(r_f));
+%      hold on
 end
