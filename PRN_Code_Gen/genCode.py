e826cf6818c89684fe0ca84ab93c04e79ec6bbec
ajinkyagorad <ajinkyagorad@gmail.com>
2016-06-07 22:23:25 +0530

    %matplotlib problem, ipython

--- PRN_Code_Gen/genCode.py
+++ PRN_Code_Gen/genCode.py
@@ -1,5 +1,42 @@
 #referenced https://natronics.github.io/blag/2014/gps-prn/
 
+# list for PRN assign for GPS spec
+
+
+SV = {
+   1: [2,6],
+   2: [3,7],
+   3: [4,8],
+   4: [5,9],
+   5: [1,9],
+   6: [2,10],
+   7: [1,8],
+   8: [2,9],
+   9: [3,10],
+  10: [2,3],
+  11: [3,4],
+  12: [5,6],
+  13: [6,7],
+  14: [7,8],
+  15: [8,9],
+  16: [9,10],
+  17: [1,4],
+  18: [2,5],
+  19: [3,6],
+  20: [4,7],
+  21: [5,8],
+  22: [6,9],
+  23: [1,3],
+  24: [4,6],
+  25: [5,7],
+  26: [6,8],
+  27: [7,9],
+  28: [8,10],
+  29: [1,6],
+  30: [2,7],
+  31: [3,8],
+  32: [4,9],
+}
 
 # let function do it
 def shift(register, feedback, output):
@@ -32,16 +69,44 @@ def shift(register, feedback, output):
 
 
 
-# init registers
-G1 = [1 for i in range(10)]
-G2 = [1 for i in range(10)]
 
-# work out first 10 bits
-ca = []
-for i in range(10):
-    g1 = shift(G1, [3,10], [10]) #feedback 3,10, output 10
-    g2 = shift(G2, [2,3,6,8,9,10], [2,6]) #feedback 2,3,6,8,9,10, output 2,6 for sat 1
-    ca.append((g1 + g2) % 2)
+def PRN(sv):
+    """Build the CA code (PRN) for a given satellite ID
+    
+    :param int sv: satellite code (1-32)
+    :returns list: ca code for chosen satellite
+    
+    """
+    
+    # init registers
+    G1 = [1 for i in range(10)]
+    G2 = [1 for i in range(10)]
 
-print ca
+    ca = [] # stuff output in here
+    
+    # create sequence
+    for i in xrange(1023):
+        g1 = shift(G1, [3,10], [10])
+        g2 = shift(G2, [2,3,6,8,9,10], SV[sv]) # <- sat chosen here from table
+        
+        # modulo 2 add and append to the code
+        ca.append((g1 + g2) % 2)
+
+    # return C/A code!
+    return ca
+
+#checking with satellite 24
+
+%matplotlib inline
+import chart
+from matplotlib import rcParams
+rcParams.update({'font.size': 12})
+
+f_prn = 10.23e6 / 10  # chipping frequency
+
+# find ca code for sat 24, and make 0 into -1 to use in BPSK
+sat_24 = [-1 if x==0 else x for x in PRN(24)]
+prn = lambda x: sat_24[int(x*f_prn)%1023]
+
+chart.Plot('GPS Sat 24 PRN Code', tend=1023/f_prn, sr=1.0e-7).signal(prn, lw=0.7).show()
 
