09beed6e5137b80127f7aa7aa0b51597bc60ef33
ajinkyagorad <ajinkyagorad@gmail.com>
2016-06-08 01:29:19 +0530

    Added smoothing at correlated data

--- DataCorrCA.m
+++ DataCorrCA.m
@@ -11,16 +11,31 @@ disp(bytes/1024/1024 );
 k = waitforbuttonpress;
 data=fread(fID,readSize);
 A = data';
-%A = split_vect(data,2);
+% B has the complex actual sampled data by SDR/USRP
 B = A(:,1)+i*A(:,2);
-figure(2)
+figure(1)
 title('Correlation with code')
 xlabel('index')
 ylabel('correlated value')
-% B has the data
+figure(2)
+title('Filtered Response')
+xlabel('index')
+ylabel('avg Value')
+%Add a filter parameters
+a = 1;
+N = 300;
+b = ones(1,N)/N;
+
 for k=1:3
     g = cacode([k],10/1.023);   % doing at 10Mhz, c/a code sampling
-    r[k] = xcorr(B,g);
-    plot(abs(r[k]));
+    r = xcorr(B,g);
+    %r_f= filter(b,a,abs(r));
+    r_f = smooth(r,N);
+    figure(1);
+    plot(abs(r));
+    hold on
+    
+    figure(2)
+    plot(abs(r_f));
     hold on
 end
