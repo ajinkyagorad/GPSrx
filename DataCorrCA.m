23dc052234c2dfac0d9d9e6c012901e5a6e065a1
ajinkyagorad <ajinkyagorad@gmail.com>
2016-06-08 17:39:12 +0530

    parametrised

--- DataCorrCA.m
+++ DataCorrCA.m
@@ -1,9 +1,14 @@
 %Correlate GPS data with the C/A codes
-% read data from file
 clear
+%parameteres
+sampling_rate_Mhz =10;
+time = 50E-3;
+ca_codes = [1 2 3 4];
+smoothingN = 1000;
+% read data from file
 fID=fopen('data');
 %1.79 GB ~ 20 sec
-time = 500E-3;
+
 bytesPerSec= (1.79*1024*1024*1024)/20;
 bytes = time*bytesPerSec;
 readSize = [2 floor(bytes/2)];
@@ -19,13 +24,14 @@ B = A(:,1)+i*A(:,2);
  xlabel('index')    
  ylabel('avg Value')
 %Add a filter parameters
-N = 1000;
-for k=10
-    g = cacode([k],10/1.023);   % doing at 10Mhz, c/a code sampling
+
+for kk = 1:length(ca_codes)
+    k = ca_codes(kk);
+    g = cacode([k],sampling_rate_Mhz/1.023);   % doing at 10Mhz, c/a code sampling
     g20 = repmat(g,1,20);
     r = xcorr(B,g20);
     figure(2);
-    r_s = smooth(r,N);
+    r_s = smooth(r,smoothingN);
     plot(abs(r_s));
     hold on
 end
