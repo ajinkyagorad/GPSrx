f257b3749aad55a7e145088c4f00f4b895c0706f
ajinkyagorad <ajinkyagorad@gmail.com>
2016-06-11 16:22:58 +0530

    corrected and working

--- xcorrl.m
+++ xcorrl.m
@@ -1,8 +1,10 @@
 function z = xcorrl(x,y)
-%correlate with y, with y being the smaller vector
+%correlate x with y, with y being the smaller vector
+%x,y,z are row vectors
 lx = length(x);
 ly = length(y);
+
     for k=1:lx-ly+1
-        z(:,k) = x(:,k:k+ly).*y;
+        z(k) = x(k:k+ly-1)*y';
     end
 end
