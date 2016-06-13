b0770ca367c4a21f42855e15df9e972741b94433
ajinkyagorad <ajinkyagorad@gmail.com>
2016-06-13 16:57:28 +0530

    added time  axis , and a custom correlation function which gives the length of output equal to that of input(the one being larger,the first argument

--- cdmaGPSsim.m
+++ cdmaGPSsim.m
@@ -2,6 +2,8 @@
 D = [ 1 0 0 1 0 1 0 1]; % data to be sent
 G = 2*(cacode([3])-0.5); %  spreading code
 mD=0; %modulated data
+chipFreq = 1.023E6;
+
 for i=1:length(D)
     if(D(i))
         bitSpread = G;
@@ -10,5 +12,7 @@ for i=1:length(D)
     end
     mD = [mD bitSpread];
 end
-y = xcorr(mD,G);
-plot((y))
+t = [1:length(mD)]/chipFreq;
+y = xcorrlx(mD,G);
+
+plot(t,y)
