b25a2b2c49fd0d449e2677557dbd48fb86af8150
ajinkyagorad <ajinkyagorad@gmail.com>
2016-06-13 16:07:06 +0530

    simulated CDMA with  CA codes, without sinc filter

---
+++ cdmaGPSsim.m
@@ -0,0 +1,14 @@
+%simulate the gps signal received by the GPS
+D = [ 1 0 0 1 0 1 0 1]; % data to be sent
+G = 2*(cacode([3])-0.5); %  spreading code
+mD=0; %modulated data
+for i=1:length(D)
+    if(D(i))
+        bitSpread = G;
+    else
+        bitSpread = -G;
+    end
+    mD = [mD bitSpread];
+end
+y = xcorr(mD,G);
+plot((y))
