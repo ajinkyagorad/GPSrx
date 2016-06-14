12a5506a6cf44c844280a8a3ac2b414595672660
ajinkyagorad <ajinkyagorad@gmail.com>
2016-06-14 18:02:55 +0530

    changed split_vectoor  to matrix read

--- autoCorr.m
+++ autoCorr.m
@@ -1,7 +1,23 @@
-fID=fopen('data');
-readSize = 1E5;
+clear
+
+
+time = 100E-3;
+fID=fopen('data');%1.79 GB ~ 20 sec
+    
+bytesPerSec= (1.79*1024*1024*1024)/20;
+bytes = time*bytesPerSec;
+readSize = [2 floor(bytes/2)];
+disp('Read Size(MB): ');
+disp(bytes/1024/1024);
+k = waitforbuttonpress;
 data=fread(fID,readSize);
-A = split_vect(data,2);
-B = A(:,1)+i*A(:,2);
-y = autocorr(B,10000);
-plot(abs(y))
+A = data';
+                
+B = A(:,1)+i*A(:,2);% B has the complex actual sampled data by SDR/USRP
+B = B';
+B = abs(B);
+figure(1)
+plot(abs(fft(B)))
+y = xcorr(B,B);
+figure(2)
+plot(y);
