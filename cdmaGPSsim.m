0c62d33a51119c2295d4965a37e40e1173c62345
Ajinkya Gorad <ajinkyagorad@yahoo.co.in>
2016-06-14 22:03:40 +0530

    updated file

--- cdmaGPSsim.m
+++ cdmaGPSsim.m
@@ -1,9 +1,10 @@
 %simulate the gps signal received by the GPS
 D = [ 1 0 0 1 0 1 0 1]; % data to be sent
-G = 2*(cacode([3])-0.5); %  spreading code
+ca = 3;
+G = 2*(cacode([ca])-0.5); %  spreading code
 mD=0; %modulated data
 chipFreq = 1.023E6;
-
+leg = 'D = ';
 for i=1:length(D)
     if(D(i))
         bitSpread = G;
@@ -11,10 +12,13 @@ for i=1:length(D)
         bitSpread = -G;
     end
     mD = [mD bitSpread]; % concatenate the signal
+    leg = [leg num2str(D(i)) ' ']
 end
-
+leg = [leg '@g' num2str(ca)]
 t = [1:length(mD)]/chipFreq;
 y = xcorrlx(mD,G);
-
-
 plot(t,y);
+legend(leg);
+title('Correlation with spreaded signal')
+xlabel('index')
+ylabel('value')
