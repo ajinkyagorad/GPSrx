0b53e5e7368b14a15c7e2fd87b488c129eb05720
Ajinkya Gorad <ajinkyagorad@yahoo.co.in>
2016-06-19 03:39:52 +0530

    added step argument for lower processing

--- Models/xcorrl.m
+++ Models/xcorrl.m
@@ -1,10 +1,10 @@
-function z = xcorrl(x,y)
+function z = xcorrl(x,y,step)
 %correlate x with y, with y being the smaller vector
 %x,y,z are row vectors
 lx = length(x);
 ly = length(y);
 
-    for k=1:1000:lx-ly+1
+    for k=1:step:lx-ly+1
         z(ceil(k/1000)) = x(k:k+ly-1)*y';
     end
 end
