78d8f091c483497dd34a991b75a5cd4f312a6692
Ajinkya Gorad <ajinkyagorad@yahoo.co.in>
2016-06-21 15:17:17 +0530

    updated

--- DataCorrCA.m
+++ DataCorrCA.m
@@ -3,7 +3,7 @@ clear
 %parameteres
 sampling_rate_Mhz =10;
 time = 100E-3;
-ca_codes =[1:37];
+ca_codes =[14 27];
 smoothingN = 1E4;
 % read data from file
 fID=fopen('data');
@@ -20,8 +20,9 @@ A = data';
 % B has the complex actual sampled data by SDR/USRP
 B = A(:,1)+i*A(:,2);
 B = B';
+B = abs(B); % merge both in phase and quadrature component into one
 
- B = decimate(B,5);sampling_rate_Mhz=2; %Add a filter parameters
+ %B = decimate(B,5);sampling_rate_Mhz=2; %Add a filter parameters
  leg  = cell(1,length(ca_codes));
  figure(2)
  hold all;
@@ -29,14 +30,14 @@ for kk = 1:length(ca_codes)
     k = ca_codes(kk);
     g = 2*(cacode([k],sampling_rate_Mhz/1.023)-0.5);   % doing at 10Mhz, c/a code sampling
     g20 = repmat(g,1,20);   % repeat matrix g 20 times gggg....ggg
-    r = xcorrl(B,g20);
+    r = xcorr(B,g20);
     %figure(2);
     %f = fft(r);
     %r_s = smooth(r,smoothingN);    
     leg{kk}=['CA',num2str(k)];
-    plot(abs(r),'LineWidth',1);
+    plot(r);
 end
 title('Response')
 xlabel('index')    
-ylabel('avg Value')
+ylabel(' Value')
 legend(leg)
