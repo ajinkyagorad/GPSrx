cc52f3ed4de806aa6cc92b5a225037e5c8e1a8cd
Ajinkya Gorad <ajinkyagorad@yahoo.co.in>
2016-06-25 20:59:49 +0530

    complexx exp mult try

--- ReceiveTransmitTest/ReadDataGNU.m
+++ ReceiveTransmitTest/ReadDataGNU.m
@@ -1,5 +1,15 @@
 clear
 fid = fopen('FMsink');
-data = fread(fid,[2 4*100],'float32');
+data = fread(fid,[2 2000],'float32');
 r = data(1,:);
-i = data(2,:);
+img = data(2,:);
+c = r+i*img;
+
+for theta=0:0.1:6.28*10
+    cor = c*exp(i*theta);
+    plot(real(cor))
+    hold on
+    plot(imag(cor))
+    hold off
+    pause(0.1);
+end
