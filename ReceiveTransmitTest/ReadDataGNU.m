f9dac9fc9831fc1be9dc88861f188bc2908ed3a0
Ajinkya Gorad <ajinkyagorad@yahoo.co.in>
2016-06-26 17:54:17 +0530

    rxdata , input: complex GNU radio Bin file, output: inphase 1bit sampled data

--- ReceiveTransmitTest/ReadDataGNU.m
+++ ReceiveTransmitTest/ReadDataGNU.m
@@ -1,12 +1,11 @@
 clear
-fid = fopen('FMsink');
- bytes = getfield(dir('FMsink'), 'bytes');
+fid = fopen('SimGPSsink');
+ bytes = getfield(dir('SimGPSsink'), 'bytes');
 data = fread(fid,[2 bytes/4/2],'float32');
 I = data(1,:);
-Ib = sign(I);
+Ib =((int8(sign(I))+1)/2)+48; %convert to byte, +1 -1 and 0;
 Q = data(2,:);
 Qb = sign(Q);
-data = Ib+Qb*i;
 
 fWriteReal = fopen('r.bin','w');
 fwrite(fWriteReal,Ib);
