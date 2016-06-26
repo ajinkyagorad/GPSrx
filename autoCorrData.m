6113ea2ff636d04cc8c5667cdb46b6175651cd0d
Ajinkya Gorad <ajinkyagorad@yahoo.co.in>
2016-06-26 15:35:40 +0530

    corrected for float32 format

--- autoCorrData.m
+++ autoCorrData.m
@@ -1,9 +1,10 @@
-    fID=fopen('data');
+
+fID=fopen('data');
 readSize = [2 2E6];
-data=fread(fID,readSize);
+data=fread(fID,readSize,'float32');
 A = data';
 B = A(:,1)+i*A(:,2);    
 y = xcorr(B,B);
-ys=smooth(y,1000);
-r = y(2.5E6:3.5E6)-ys(2.5E6:3.5E6);
-plot(abs(r))
+%ys=smooth(y,1000);
+%r = y(2.5E6:3.5E6)-ys(2.5E6:3.5E6);
+plot(abs(y))
