2eeb5261e096bbec9d67890bdebc77fa5a087c7d
Ajinkya Gorad <ajinkyagorad@yahoo.co.in>
2016-06-28 18:16:05 +0530

    G = g20, commented

--- cdmaGPSsim.m
+++ cdmaGPSsim.m
@@ -1,7 +1,8 @@
 %simulate the gps signal received by the GPS
 D = [ 1 0 0 1 0 1 0 1 1 1 1 1 0 0 0 0 0]; % data to be sent
 ca = 3;
-G = 2*(cacode([ca],10)-0.5); %  spreading code
+g = 2*(cacode([ca],10)-0.5); %  spreading code
+G = repmat(g,[1 20]); % repeat 20 times CA code, 1 data bit
 mD=0; %modulated data
 chipFreq = 1.023E6;
 leg = 'D = ';
@@ -17,10 +18,14 @@ end
 leg = [leg '@g' num2str(ca)]
 t = [1:length(mD)]/chipFreq;
 [p q] = size(mD);
-mDs =0.2*mD+wgn(p,q,0);
-y = xcorrlx(mDs,G,1);
+mDs =0.1*mD+wgn(p,q,0);
+mDs = sin(2*3.14*10*[1:length(mDs)]).*mDs;
+%% now correlate signal with the code
+%y = xcorrlx(mDs,G,50); % get cross correlation
+y = xcorr(mDs,mDs);
 plot(y);
 legend(leg);
 title('Correlation with spreaded signal')
 xlabel('index')
 ylabel('value')
+
